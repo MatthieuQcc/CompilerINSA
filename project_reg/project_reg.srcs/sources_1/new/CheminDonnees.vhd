a1b1afa08a0c7e2ca5f5c7a827de91c4c8605d64
Oriane <orianeberry4@gmail.com>
2022-06-01 09:43:08 +0200

    fin chemin donnees

--- project_reg/project_reg.srcs/sources_1/new/CheminDonnees.vhd
+++ project_reg/project_reg.srcs/sources_1/new/CheminDonnees.vhd
@@ -128,6 +128,10 @@ architecture Behavioral of CheminDonnees is
     signal MUX_S : STD_LOGIC_VECTOR (7 downto 0) := (others => '0');
     signal LC_EX : STD_LOGIC_VECTOR (1 downto 0) := (others => '0');
     
+    --Quatrieme etage
+    signal MUX_OUT_MD : STD_LOGIC_VECTOR (7 downto 0) := (others => '0');
+    signal LC_MEM : STD_LOGIC := '0';
+    signal MUX_IN_MD : STD_LOGIC_VECTOR (7 downto 0) := (others => '0');
     
     begin
     
@@ -159,8 +163,16 @@ architecture Behavioral of CheminDonnees is
         S => MUX_S
         );
     
-    --mem_donnees : memoire_donnees port Map (
-    --    );
+    mem_donnees : memoire_donnees port Map (
+        add => EX_MEM_B,
+        input => MUX_IN_MD,
+        RW => LC_MEM,
+        RST => RST_cpu,
+        CLK => CLK_cpu,
+        output => MUX_OUT_MD
+        );
+    
+    --Place les données d'instruction dans le pipeline LI/DI
     process
     begin
     --Premier etage 
@@ -169,33 +181,86 @@ architecture Behavioral of CheminDonnees is
     LI_DI_OP <= output_mi(31 downto 24);
     LI_DI_B <= output_mi(15 downto 8);
     LI_DI_C <= output_mi(7 downto 0);
-   
-    --Deuxieme etage 
+    end process;
+    
+   --propage les données dans le pipeline DI/EX
+   process
+   begin
     DI_EX_A <= LI_DI_A;
     DI_EX_OP <= LI_DI_OP;
+    end process;
     
-    if LI_DI_OP = x"05" then 
+    --gere l'instruction AFC
+    process
+    begin
+    if LI_DI_OP = x"5" then 
     --Si instruction AFC
         DI_EX_B <= LI_DI_B ;
     else 
     --Pour toutes les autres instructions
-        DI_EX_B  <= MUX_QA;
+        DI_EX_B  <= MUX_QA; --permet de coder COP
+    end if;
+    end process;
+    
+    
+    --propage les données dans le pipeline EX/MEM
+    process
+    begin
+    EX_MEM_A <= DI_EX_A;
+    EX_MEM_OP <= DI_EX_OP;
+    if DI_EX_OP < x"4" or DI_EX_OP > x"0" then
+        LC_EX <= DI_EX_OP;
+    else 
+        LC_EX <= x"0";
+    end if;
+    end process;
+    
+    process
+    begin
+    --Cas des instructions ADD,MUL,SOU
+    if (EX_MEM_OP < x"4") or (EX_MEM_OP > x"0") then 
+        EX_MEM_B <= MUX_S;
+    else
+    --Pour le reste des instructions 
+        EX_MEM_B <= DI_EX_B;    
+    end if;
+    end process;
+    
+    --propage les données dans le pipeline MEM/RE
+    process
+    begin
+        MEM_RE_A <= EX_MEM_A;
+        MEM_RE_OP <= EX_MEM_OP; 
+        if EX_MEM_OP = x"8" then 
+            LC_MEM <= '0';
+        else 
+            LC_MEM <= '1';
+        end if;
+    end process;
+    
+    process
+    begin
+    
+    if MEM_RE_OP = x"7" then 
+        MEM_RE_B <= MUX_OUT_MD;
+    else
+        MEM_RE_B <= EX_MEM_B;
     end if;
     
+    if MEM_RE_OP = x"8" then 
+        MUX_IN_MD <= EX_MEM_A;
+    else
+        MUX_IN_MD <= EX_MEM_B;
+    end if;
     
-    --Troisieme etage
-    EX_MEM_A <= DI_EX_A;
-    EX_MEM_OP <= DI_EX_OP; 
-    
-    if 
-    EX_MEM_B <= DI_EX_B;
-    
-    --Quatrieme etage
-    MEM_RE_A <= EX_MEM_A;
-    MEM_RE_OP <= EX_MEM_OP;    
-    MEM_RE_B <= EX_MEM_B;
-    
+    if MEM_RE_OP = x"0" or MEM_RE_OP = x"8" then
+        LC_RE <= '0';
+    else 
+        LC_RE <= '1';
+    end if;
     end process;
+    
+    
     
     
     
