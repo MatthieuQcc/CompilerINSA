c7e285ed1ca7ffe9d3215a7a3f7fc57a8acbf7a4
Oriane <orianeberry4@gmail.com>
2022-06-01 11:11:22 +0200

    avancement table instruction

--- table_instructions.c
+++ table_instructions.c
@@ -5,8 +5,18 @@
 my_instr tabInstr [MAX_INSTR];
 int tab_index = 0;
 
-
+//Instructions : ADD, MUL, SOU, DIV, COP, AFC, JMP, JMF, INF, SUP, EQU, PRI
 
 void addInstrToTable( char* operation, int r0, int r1, int r2){
-    NULL;
+    
+    my_instr instruction = {.operation="operation",.r0=r0,.r1=r1,.r2=r2};
+    tabInstr[tab_index] = instruction;
+    tab_index ++;
+
 }
+
+int get_index_last_instr() {
+    return tab_index;
+}
+
+
